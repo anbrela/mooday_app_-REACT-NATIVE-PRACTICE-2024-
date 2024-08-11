cfca8db34ecdabb4f2dfb6d5e0facb279e86368b
Pablo Naveira López <pablonaveiralopez@gmail.com>
2024-08-11 18:06:12 +0200

    feat: initial commit app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,37 @@
-import { useTranslations } from "@/hooks/useTranslations";
-import { SafeAreaView, View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import { View, Text, SafeAreaView, Button, Platform } from "react-native";
 import { Image } from "expo-image";
+import { useTranslations } from "@/hooks/useTranslations";
 
 export default function HomeScreen() {
+  const [userInfo, setUserInfo] = useState(null);
   const { t } = useTranslations();
+
+  const redirectUri = Platform.select({
+    web: window?.location?.origin,
+    default: `https://auth.expo.io/@pablonaveira/mooday`, // Ajusta este valor según tu configuración
+  });
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "128876708418-a8fe278bshvimcn75f9tk5ah158n2fub.apps.googleusercontent.com",
+    iosClientId:
+      "128876708418-ef46h5khhd5noil2i9llef07bdfaqjg0.apps.googleusercontent.com",
+    androidClientId: "YOUR_ANDROID_CLIENT_ID",
+    webClientId:
+      "128876708418-a8fe278bshvimcn75f9tk5ah158n2fub.apps.googleusercontent.com",
+    redirectUri,
+    scopes: ["profile", "email"],
+  });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { authentication } = response;
+      // Aquí puedes hacer una solicitud a la API de Google con el token para obtener la información del usuario
+      console.log(authentication);
+    }
+  }, [response]);
 
   return (
     <SafeAreaView>
@@ -16,6 +44,13 @@ export default function HomeScreen() {
             source={require("@/assets/images/home.svg")}
           />
         </View>
+        <Button
+          disabled={!request}
+          title="Sign in with Google"
+          onPress={() => {
+            promptAsync();
+          }}
+        />
       </View>
     </SafeAreaView>
   );
